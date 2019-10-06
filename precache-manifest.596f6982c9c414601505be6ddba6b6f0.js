ad0bd61b22bd9e73ffa56328a74f45eb6a1de0bb
sieder <sieder00@gmail.com>
2019-10-06 12:16:08 +0300

    Updates

---
+++ precache-manifest.596f6982c9c414601505be6ddba6b6f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572fc19cd6d61a0bcc40585d05b25a38",
+    "url": "/SVtodoapp/index.html"
+  },
+  {
+    "revision": "d25a8af49eb8e1615c44",
+    "url": "/SVtodoapp/static/css/main.78d996b7.chunk.css"
+  },
+  {
+    "revision": "3c255f7447269b0c2e0a",
+    "url": "/SVtodoapp/static/js/2.65324c96.chunk.js"
+  },
+  {
+    "revision": "d25a8af49eb8e1615c44",
+    "url": "/SVtodoapp/static/js/main.fda6a439.chunk.js"
+  },
+  {
+    "revision": "71778a4fd5f10990d9cf",
+    "url": "/SVtodoapp/static/js/runtime~main.ea6f8672.js"
+  }
+]);
